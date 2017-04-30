63a56ea7a9c090ac79cdbd40e7f3061009b18dfe
Amaury Ortega <amauryocortega@gmail.com>
2017-04-30 12:06:32 -0500

    Modificacion leve

--- ejemplo1/application/views/estadios/index.php
+++ ejemplo1/application/views/estadios/index.php
@@ -1,9 +1,10 @@
 <h3><?php echo $titulo;?></h3>
 
 <?php foreach ($estadios as $estadio): ?>
-    <!--<h3><?php echo $estadio['Identificador']?></h3>-->
     <!--Donde Nombre es un atributo de la tabla en la base de datos-->
-    <div class="main"><?php echo $estadio['Nombre'];?></div>
-    <!--Deja el link ...index.php/estadios/Cartagena-->
-    <p><a href="<?php echo site_url('estadios/'.$estadio['Nombre']);?>">Ver estadio</a></p>
+    <div class="main">
+        <!--Deja el link ...index.php/estadios/Cartagena-->
+        <a href="<?php echo site_url('estadios/'.$estadio['Nombre']);?>"><?php echo '#'.$estadio['Identificador'].' ';echo $estadio['Nombre'];?></a>
+    </div>
+    <br>
 <?php endforeach; ?>
