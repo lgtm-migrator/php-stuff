31b98cf0b9e25307be8baba743d6debda2507b81
Amaury Ortega <amauryocortega@gmail.com>
2017-05-01 22:25:45 -0500

    Uso alternativo de base_url

--- ejemplo1/application/config/config.php
+++ ejemplo1/application/config/config.php
@@ -24,6 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/php-stuff/ejemplo1/';
+//Usando esta version de base_url, codeigniter detecta la ip correspondiente
+//$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
