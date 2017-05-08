c81fc3933dc53eb4f84b024f8eeb8021de6c7e57
Amaury Ortega <amauryocortega@gmail.com>
2017-05-08 16:32:50 -0500

    Links para coninuar

--- script.py
+++ script.py
@@ -6,3 +6,7 @@ akey:5f921720613d575ccd9b84123272bf8aaabbbfd6
 ikey:DI5TQ90UMLEW0194FFHB
 skey:W4Ld7PEf3UxC85PBbS1FfJm2kyD8XWH8NENvqD4N
 """
+
+https://github.com/duosecurity/duo_php/
+https://code.tutsplus.com/tutorials/integrating-two-factor-authentication-with-codeigniter--net-20667
+https://duo.com/docs/duoweb#troubleshooting
