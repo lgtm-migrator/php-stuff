273237bf3e17a3e6fb5b2b429de5519ba4d355c6
Amaury Ortega <amauryocortega@gmail.com>
2017-05-08 01:05:03 -0500

    Añadiendo seguridad a CAdmin/crearPartidos

--- beisbol_seguro/application/controllers/CAdmin.php
+++ beisbol_seguro/application/controllers/CAdmin.php
@@ -64,41 +64,49 @@ class CAdmin extends CI_Controller {
 
     public function crearPartidos($num = 1){
         //Si ya esta logeado
-        $data['error']='';
-        $data['error1']='';
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('puntos_Equipo1', 'Puntos Equipo 1', 'required|max_length[2]|is_natural', array('required' => '"El campo {field} esta vacio."','max_length' => '"El campo {field} acepta maximo 2 caracteres"','is_natural' => '"El campo {field} acepta solo numeros naturales"'));
-        $this->form_validation->set_rules('puntos_Equipo2', 'Puntos Equipo 2', 'required|max_length[2]|is_natural', array('required' => '"El campo {field} esta vacio."','max_length' => '"El campo {field} acepta maximo 2 caracteres"','is_natural' => '"El campo {field} acepta solo numeros naturales"'));
+        if (!isset($_SESSION['username'])){
+            //de regreso a login
+            redirect('admin');
+        }else {
+            if($num != 1 and $num != 2 and $num != 3 and $num != 4){
+                //Se intenta pasar de listo
+                redirect('admin/crear-partidos');
+            }
+            $data['error']='';
+            $data['error1']='';
+            $this->load->library('form_validation');
+            $this->form_validation->set_rules('puntos_Equipo1', 'Puntos Equipo 1', 'required|max_length[2]|is_natural', array('required' => '"El campo {field} esta vacio."','max_length' => '"El campo {field} acepta maximo 2 caracteres"','is_natural' => '"El campo {field} acepta solo numeros naturales"'));
+            $this->form_validation->set_rules('puntos_Equipo2', 'Puntos Equipo 2', 'required|max_length[2]|is_natural', array('required' => '"El campo {field} esta vacio."','max_length' => '"El campo {field} acepta maximo 2 caracteres"','is_natural' => '"El campo {field} acepta solo numeros naturales"'));
 
-        if ($this->form_validation->run() !== false) {
-            //Valido correctamente. Obtener de la BD los credenciales
-            if($this->input->post('equipo1')===$this->input->post('equipo2')){
-                $data['error'] = 'No puede jugar un equipo contra el mismo.';
-            }else{
-                $limitePartido = $this->MAdmin->validarCinco($this->input->post('equipo1'),$this->input->post('equipo2'));
-                if($limitePartido !== NULL){
-                    $data['error1'] = 'El equipo '.$limitePartido.' ha llegado al limite de partidos jugados.';
+            if ($this->form_validation->run() !== false) {
+                //Valido correctamente. Obtener de la BD los credenciales
+                if($this->input->post('equipo1')===$this->input->post('equipo2')){
+                    $data['error'] = 'No puede jugar un equipo contra el mismo.';
                 }else{
-                    $data = array(
-                    'equipo1' => $this->security->xss_clean($this->input->post('equipo1')),
-                    'equipo2' => $this->security->xss_clean($this->input->post('equipo2')),
-                    'puntos_Equipo1' => $this->security->xss_clean($this->input->post('puntos_Equipo1')),
-                    'puntos_Equipo2' => $this->security->xss_clean($this->input->post('puntos_Equipo2')),
-                    'estadio' => $this->security->xss_clean($this->input->post('estadio')),
-                    );
-                    $this->MAdmin->crearPartido($data);
-                    redirect('admin/crear-partidos');
+                    $limitePartido = $this->MAdmin->validarCinco($this->input->post('equipo1'),$this->input->post('equipo2'));
+                    if($limitePartido !== NULL){
+                        $data['error1'] = 'El equipo '.$limitePartido.' ha llegado al limite de partidos jugados.';
+                    }else{
+                        $data = array(
+                        'equipo1' => $this->security->xss_clean($this->input->post('equipo1')),
+                        'equipo2' => $this->security->xss_clean($this->input->post('equipo2')),
+                        'puntos_Equipo1' => $this->security->xss_clean($this->input->post('puntos_Equipo1')),
+                        'puntos_Equipo2' => $this->security->xss_clean($this->input->post('puntos_Equipo2')),
+                        'estadio' => $this->security->xss_clean($this->input->post('estadio')),
+                        );
+                        $this->MAdmin->crearPartido($data);
+                        redirect('admin/crear-partidos');
+                    }
                 }
             }
+
+            $data['division'] = $num;
+            $data['equipos'] = $this->MEquipos->listaEquipos($num);
+            $data['estadios'] = $this->MAdmin->listaEstadios();
+            $this->load->view('admin/header');
+            $this->load->view('admin/crearPartido',$data);
+            $this->load->view('plantillas/footer');
         }
-
-        $data['division'] = $num;
-        $data['equipos'] = $this->MEquipos->listaEquipos($num);
-        $data['estadios'] = $this->MAdmin->listaEstadios();
-        $this->load->view('admin/header');
-        $this->load->view('admin/crearPartido',$data);
-        $this->load->view('plantillas/footer');
-
     }
 
     public function editarJugador(){
