01261d214dd902b7837b3fdf3761a3b9fcefa160
Amaury Ortega <amauryocortega@gmail.com>
2017-05-08 14:50:40 -0500

    IDs de 2nd auth

---
+++ script.py
@@ -0,0 +1,8 @@
+import os, hashlib
+print hashlib.sha1(os.urandom(32)).hexdigest()
+
+"""
+akey:5f921720613d575ccd9b84123272bf8aaabbbfd6
+ikey:DI5TQ90UMLEW0194FFHB
+skey:W4Ld7PEf3UxC85PBbS1FfJm2kyD8XWH8NENvqD4N
+"""
